b5e8e554301bddd736bff76990f19aa462ba4687
Temam-Hashim <temamhashim3@gmail.com>
2023-08-31 15:42:10 +0300

    project added

--- src/components/Work.js
+++ src/components/Work.js
@@ -19,6 +19,7 @@ const workData = [
     technologies: ["HTML", "JS", "PHP"],
     budget: "200 USD",
     url: "https://doctors.damichaa.com",
+    desc:"Tele Doctor is a concise and informative medical website offering expert health advice, tips, and insights. Visitors can access a wide range of articles and resources covering various medical topics, wellness recommendations, treatment options, and preventive measures. With user-friendly navigation and trustworthy content, Doctor\'s Tip aims to empower individuals to make informed decisions about their health and well-being"
   },
   {
     img: "img/projects/tele-automotive.png",
@@ -30,6 +31,7 @@ const workData = [
     technologies: ["HTML", "JS", "PHP"],
     budget: "200 USD",
     url: "https://automotive.welmaan.com/",
+    desc:"Explore our automotive tip website for expert advice and practical insights on all things automotive. From maintenance and repairs to buying guides and driving tips, we offer a comprehensive resource to help you navigate the world of cars with confidence"
   },
 
   {
@@ -42,6 +44,7 @@ const workData = [
     technologies: ["HTML", "JS", "PHP"],
     budget: "200 USD",
     url: "https://lawyers.damichaa.com",
+    desc:"Welcome to our lawyer tip website, where legal guidance meets practical advice. We provide concise and insightful tips on a variety of legal topics to help you navigate the complexities of the law with confidence. Our expert tips cover everything from understanding your rights to handling legal paperwork and making informed decisions. Whether you're facing a legal challenge or simply want to be better informed, our website is your go-to resource for clear and actionable legal insights."
   },
   {
     img: "img/projects/tele-pharmacy.png",
@@ -53,6 +56,7 @@ const workData = [
     technologies: ["HTML", "JS", "PHP"],
     budget: "200 USD",
     url: "https://pharma.meetconsultancy.com",
+    desc:"Explore our pharmacy medical tip website for concise and valuable insights. Discover a range of health-related tips, medication guidance, and wellness advice curated by healthcare professionals. Enhance your understanding of medical topics and make informed decisions about your health and well-being."
   },
   {
     img: "img/projects/tele-entrepreneur.png",
@@ -64,6 +68,7 @@ const workData = [
     technologies: ["HTML", "JS", "PHP"],
     budget: "200 USD",
     url: "https://entrepreneur.welmaan.com",
+    desc:"Empowering Entrepreneurs is a website dedicated to equipping and motivating aspiring and seasoned entrepreneurs. Our platform offers a concise and insightful collection of tips, strategies, and resources to help entrepreneurs navigate challenges, seize opportunities, and drive their ventures toward success"
   },
 
 
@@ -77,6 +82,8 @@ const workData = [
     technologies: ["HTML", "JS", "PHP"],
     budget: "350 USD",
     url: "http://learn.coopbank.local/",
+    desc:"An e-Learning system website is a digital platform designed to facilitate online education and learning experiences. It provides a virtual environment where students and educators can interact, access educational resources, and participate in various educational activities. "
+
   },
   {
     img: "img/projects/los_1.png",
@@ -88,6 +95,7 @@ const workData = [
     technologies: ["HTML", "JAVASCRIPT", "REACT", "NODEJS", "PYTHON", "JAVA"],
     budget: "350 USD",
     url: "http://apps.coopbank.local/LOS/",
+    desc:"A Loan Origination System (LOS) website is an online platform that facilitates the process of applying for and processing loans. It provides a user-friendly interface for borrowers, lenders, and loan officers to interact and manage various stages of the loan application, approval, and disbursal process."
   },
   {
     img: "img/projects/legal_request_1.png",
@@ -99,6 +107,7 @@ const workData = [
     technologies: ["HTML", "JAVASCRIPT", "REACT", "NODEJS", "PYTHON", "JAVA"],
     budget: "350 USD",
     url: "http://apps.coopbank.local/legal/",
+    desc:"The Bank Legal Request System is a dedicated website that serves as a secure and streamlined platform for managing legal requests and inquiries for a bank. This online system facilitates communication and collaboration between the bank's legal team and external parties such as law enforcement agencies, regulatory authorities, and legal representatives."
   },
   {
     img: "img/projects/equb_1.png",
@@ -110,6 +119,7 @@ const workData = [
     technologies: ["HTML", "JS", "REACT", "NODEJS", "JAVA"],
     budget: "1000 USD",
     url: "#",
+    desc:"Digital EQUB is an online platform that offers a diverse collection of digital books, magazines, and educational materials. With a user-friendly interface, it provides access to a wide range of digital content spanning various genres, catering to readers of all ages. From classic literature to contemporary publications, Digital EQUB aims to enhance reading experiences in the digital age, promoting learning and entertainment through its extensive digital library."
   },
   {
     img: "img/projects/doctor_1.png",
@@ -121,6 +131,7 @@ const workData = [
     technologies: ["HTML", "JS", "PHP", "MYSQL", "BOOTSTRAP"],
     budget: "450 USD",
     url: "https://doctors.black-fs.com/",
+    desc:"Meet Doctor is a concise and informative medical website offering expert health advice, tips, and insights. Visitors can access a wide range of articles and resources covering various medical topics, wellness recommendations, treatment options, and preventive measures. With user-friendly navigation and trustworthy content, Doctor\'s Tip aims to empower individuals to make informed decisions about their health and well-being"
   },
   {
     img: "img/projects/lawyer_1.png",
@@ -132,6 +143,7 @@ const workData = [
     technologies: ["HTML", "JS", "PHP", "MYSQL", "BOOTSTRAP"],
     budget: "300 USD",
     url: "https://meetlawyer.black-fs.com/",
+    desc:"Welcome to our lawyer tip website, where legal guidance meets practical advice. We provide concise and insightful tips on a variety of legal topics to help you navigate the complexities of the law with confidence. Our expert tips cover everything from understanding your rights to handling legal paperwork and making informed decisions. Whether you're facing a legal challenge or simply want to be better informed, our website is your go-to resource for clear and actionable legal insights."
   },
   {
     img: "img/projects/OMS.png",
@@ -143,6 +155,7 @@ const workData = [
     technologies: ["HTML", "JS", "PHP", "MYSQL", "JQUERY", "BOOTSTRAP"],
     budget: "300 USD",
     url: "http://black.is-best.net/Black/",
+    desc:"An Office Management System website is designed to streamline and optimize administrative tasks and processes within an office environment. It serves as a digital platform that assists in managing various aspects of office operations efficiently."
   },
   {
     img: "img/projects/social_1.png",
@@ -154,6 +167,7 @@ const workData = [
     technologies: ["HTML", "JS", "REACT", "NODEJS", "MONGODB"],
     budget: "200 USD",
     url: "#",
+    desc:"Discover a vibrant online community on our social media app website. Connect with friends, share your thoughts, and explore a world of photos, videos, and stories. Stay updated with real-time notifications, engage in meaningful conversations, and express yourself creatively. Join us today to experience a platform that brings people together, fostering connections and conversations in an innovative and user-friendly environment."
   },
   {
     img: "img/projects/e-commerce_2.png",
@@ -165,6 +179,7 @@ const workData = [
     technologies: ["HTML", "JS", "PHP", "MYSQL", "BOOTSTRAP"],
     budget: "200 USD",
     url: "#",
+    desc:"An e-commerce website is an online platform where businesses or individuals can display and sell products or services to customers over the internet. It allows users to browse through a wide range of items, add them to a virtual shopping cart, and proceed to checkout for secure online payment."
   },
 ];
 
@@ -207,7 +222,8 @@ const Work = () => {
                           className="img-fluid"
                         />
                         <div>
-                          <span>{work.name}</span>
+                          {/* <span>{work.name}</span><br/><hr/> */}
+                          <span style={{fontSize:"8px"}}>{work.desc}</span>
                         </div>
                       </a>
                       <div
